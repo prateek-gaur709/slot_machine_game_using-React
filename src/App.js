b0db23006892f0ca5dd374fdab18ff11e7c03e05
Prateek Kumar Gaur <ssprateekgaur999@gmail.com>
2023-02-23 11:17:27 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const SlotM = (props) => {
+  // let x = props.x;
+  // let y = props.y;
+  // let z = props.z;
+
+  //object destructuring
+  let { x, y, z } = props;
+
+  var res = '';
+  x === y && y === z
+    ? (res = 'Hurrah!! You are winner!!')
+    : (res = 'Alas!! You are not a winner! ');
+
+  return (
+    <>
+      <div className='slot_inner'>
+        <h1>
+          {props.x} {props.y} {props.z}
+        </h1>
+        <h1>{res}</h1>
+        <hr />
+      </div>
+    </>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='heading_style'>
+        🎰 Welcome to
+        <span style={{ fontWeight: 'bold' }}>Slot Machine Game </span> 🎰
+      </h1>
+      <div className='slotMachine'>
+        <SlotM x='❤️' y='❤️' z='🙋‍♂️' />
+        <SlotM x='❤️' y='❤️' z='❤️' />
+        <SlotM x='😂' y='❤️' z='🙋‍♂️' />
+        <SlotM x='😊' y='😊' z='😊' />
+      </div>
+    </>
   );
 }
 
